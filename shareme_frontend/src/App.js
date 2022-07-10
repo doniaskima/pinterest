a19239402d6aaa55b3dacca26b4594bde84ce902
doniaskima <doniaskima344@gmail.com>
2022-07-10 13:09:15 +0100

    still fixing

--- shareme_frontend/src/App.js
+++ shareme_frontend/src/App.js
@@ -3,16 +3,13 @@ import { Routes, Route, useNavigate } from 'react-router-dom';
 import Login from './components/Login';
 import Home from './container/Home';
 const App = () => {
-    return ( < Routes >
-        <
-        Route path = "login"
-        element = { < Login / > }
-        /> <
-        Route path = "/*"
-        element = { < Home / > }
-        /> < /
-        Routes >
-    );
+  
+    return (
+    <Routes>
+      <Route path="Login" element={<Login />} />
+      <Route path="/*" element={<Home />} />
+    </Routes>
+  );
 }
 
 export default App;
